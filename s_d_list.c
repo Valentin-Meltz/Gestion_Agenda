6b21e3c6669265539da9581b159dbb9abf45abb1
Meltz <valentin.meltz@efrei.net>
2023-10-23 11:46:26 +0200

    Commit_test

---
+++ s_d_list.c
@@ -0,0 +1,5 @@
+//
+// Created by Valentin Meltz on 23/10/2023.
+//
+
+#include "s_d_list.h"
---
+++ s_d_list.h
@@ -0,0 +1,30 @@
+#ifndef GESTION_AGENDA_S_D_LIST_H
+#define GESTION_AGENDA_S_D_LIST_H
+
+//Structure d'une cellule à level niveau
+struct s_d_cell {
+    int value;
+    int level;
+    struct s_d_cell **next;     //Liste de tous les pointeurs next de chaque niveau
+};
+typedef struct s_d_cell t_cell, *p_cell;
+
+p_cell Create_cell(int, int);   //Création d'une cellule
+void Display_cell(p_cell);  //affichage d'une cellule
+
+
+//Structure d'une list à max_level niveau maximum
+struct s_d_list {
+    int max_level;
+    t_cell **head;      //Liste de tous les pointeurs head de chaque niveau
+};
+typedef struct s_d_list t_list;
+
+t_list Create_list(int);    //Création d'une liste vide
+void Add_Head_list(t_list*, p_cell);    //Ajout d'une cellule en tête de list
+void Display_list_level(t_list);    //Affichage d'un niveau de la liste
+void Display_All_list(t_list);    //Affichage de tous les niveaux de la liste
+void Display_All_list_aligne(t_list);   //Affichage de tous les niveau de la liste (aligné)
+void Add_cell(t_list*, p_cell);     //Ajout d'une cellule à niveau en respectant l'ordre croissant
+
+#endif //GESTION_AGENDA_S_D_LIST_H
