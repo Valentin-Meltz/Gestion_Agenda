7b014a8245b3f20e0645de0b65423d342530ff1f
Meltz <valentin.meltz@efrei.net>
2023-10-25 14:01:50 +0200

    Commit1_Partie_1

--- s_d_list.c
+++ s_d_list.c
@@ -1,5 +1,55 @@
-//
-// Created by Valentin Meltz on 23/10/2023.
-//
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "s_d_list.h"
+
+p_cell Create_cell(int level, int val){
+    p_cell cell = (p_cell) malloc(sizeof(p_cell));
+    cell->level = level;
+    cell->value = val;
+    cell->next = (p_cell *) malloc(cell->level * sizeof(p_cell));
+    for (int i = 0; i < cell->level; i++){
+        cell->next[i] = NULL;
+    }
+    return cell;
+}
+
+void Display_cell(p_cell cell){
+    printf("[ %d|@-]", cell->value);
+}
+
+t_list Create_list(int max_level){
+    t_list mylist;
+    mylist.max_level = max_level;
+    mylist.head = (p_cell *) malloc(mylist.max_level * sizeof(p_cell));
+    for (int i = 0; i < mylist.max_level; i++){
+        mylist.head[i] = NULL;
+    }
+    return mylist;
+}
+
+void Add_Head_list(t_list*, p_cell){
+
+}
+
+void Display_list_level(t_list mylist, int level){
+    printf("[list head_%d @-]-->", level);
+    if (mylist.head[level] == NULL){
+        printf("NULL\n");
+        return;
+    }
+
+    p_cell temp = mylist.head[level];
+    while(temp != NULL){
+        Display_cell(temp);
+        temp = temp->next[level];
+    }
+    printf("NULL\n");
+}
+
+void Display_All_list(t_list mylist, int level){
+    if (level != mylist.max_level)
+        Display_list_level(mylist, level++);
+}
+
+void Display_All_list_aligne(t_list);
+void Add_cell(t_list*, p_cell);
--- s_d_list.h
+++ s_d_list.h
@@ -22,8 +22,8 @@ typedef struct s_d_list t_list;
 
 t_list Create_list(int);    //Création d'une liste vide
 void Add_Head_list(t_list*, p_cell);    //Ajout d'une cellule en tête de list
-void Display_list_level(t_list);    //Affichage d'un niveau de la liste
-void Display_All_list(t_list);    //Affichage de tous les niveaux de la liste
+void Display_list_level(t_list, int);    //Affichage d'un niveau de la liste
+void Display_All_list(t_list, int);    //Affichage de tous les niveaux de la liste
 void Display_All_list_aligne(t_list);   //Affichage de tous les niveau de la liste (aligné)
 void Add_cell(t_list*, p_cell);     //Ajout d'une cellule à niveau en respectant l'ordre croissant
 
