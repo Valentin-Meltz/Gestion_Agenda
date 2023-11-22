1a7aba22765782b1ff27ca7c05b98bed90af7f1e
Z3rbe <131174390+Z3rbe@users.noreply.github.com>
2023-11-22 22:29:20 +0100

    Commit17_Part3

--- main.c
+++ main.c
@@ -5,20 +5,51 @@
 #include "contact.h"
 #include "rendez-vous.h"
 
+int SecureAdd(int Level_S)
+{
+    int val = 0;
+    switch (Level_S) {
+        case 1 : {
+            printf("1 | Mes contact.\n2 | Rechercher un contact.\n3 | Nouveau contact.\n4 | Sauvegarder et quitter.\n\n");
+            scanf("%d", &val);
+            while (val < 1 || val > 4)
+            {
+                printf("Valeur incorrecte, veuiller saisir une autre valeur : ");
+                scanf(" %d", &val);
+            }
+            return val;
+        }
+        case 2 : {
+            printf("Entrer 0 Pour revenir en arriere.\n\n");
+            scanf("%d",&val);
+            while (val != 0)
+            {
+                printf("Valeur incorrecte, veuiller saisir une autre valeur : ");
+                scanf(" %d", &val);
+            }
+            return val;
+        }
+        case 3 : {
+            printf("1 | Mes rendez-vous\n2 | Ajouter un rendez-vous\n3 | Suprimer un rendez-vous\n0 | Retour\n\n");
+            scanf("%d",&val);
+            while (val < 0 || val > 3)
+            {
+                printf("Valeur incorrecte, veuiller saisir une autre valeur : ");
+                scanf(" %d", &val);
+            }
+            return val;
+        }
+    }
+}
+
 void main_rdv(p_contact contact){
     contact->rdv = Load_rdv(contact->name);
 
     while(1) {
         printf("%s :\n", contact->name); //Fonction de Tuan pour bien afficher le contact
-        printf("A | Mes rendez-vous\nB | Ajouter un rendez-vous\nC | Suprimer un rendez-vous\nD | Retour\n\n");
-        char choix;
-        do {
-            printf("$ ");
-            scanf("%c", &choix);
-        } while (choix != 'A' && choix != 'B' && choix != 'C' && choix != 'D');
-
-        switch (choix) {
-            case 'A': {
+        int val = SecureAdd(3);
+        switch (val) {
+            case 1: {
                 //Si la liste est vide
                 if(isEmptyRdv(contact->rdv))
                     printf("Vous n'avez pas de rendez vous avec ce contact.\n");
@@ -26,17 +57,11 @@ void main_rdv(p_contact contact){
                 //Si la liste n'est pas vide
                 else DisplayL_rdv(contact->rdv);
 
-                int val1;
-                printf("Entrer 0 pour revenir en arrière.\n");
-                do {
-                    printf("$ ");
-                    scanf("%d", &val1);
-                } while (val1 != 0);
-                printf("\n\n");
+                int val1 = SecureAdd(2);
                 break;
             }
 
-            case 'B': {
+            case 2 : {
                 int jour, mois, annee, h, min, dh, dmin;
                 printf("Entrer la date (J/M/A) : ");
                 scanf("%d/%d/%d", &jour, &mois, &annee);
@@ -51,7 +76,7 @@ void main_rdv(p_contact contact){
                 break;
             }
 
-            case 'C': {
+            case 3 : {
                 int jour, mois, annee, h, min;
                 printf("Entrer la date (J/M/A) : ");
                 scanf("%d/%d/%d", &jour, &mois, &annee);
@@ -71,7 +96,7 @@ void main_rdv(p_contact contact){
                 break;
             }
 
-            default: {
+            case 0 : {
                 Save_rdv(contact->rdv, contact->name);
                 return;
             }
@@ -87,13 +112,8 @@ int main() {
     //Boucle infini
     while(1) {
         printf("\t\t__MON AGENDA__\n");
-        printf("1 | Mes contact.\n2 | Rechercher un contact.\n3 | Nouveau contact.\n4 | Sauvegarder et quitter.\n\n");
-        int val = 0;
-        do {
-            printf("$ ");
-            scanf("%d", &val);
-        } while (val < 1 || val > 4);
-
+        int Level_S = 1;
+        int val = SecureAdd(Level_S);
         switch (val) {
             case 1: {
                 // Affichage des contact
@@ -107,13 +127,7 @@ int main() {
                 else
                     DisplayAllContact(MyContactList);
 
-                int val1;
-                printf("Entrer 0 pour revenir en arrière.\n");
-                do {
-                    printf("$ ");
-                    scanf("%d", &val1);
-                } while(val1 != 0);
-                printf("\n\n");
+                int val1 = SecureAdd(2);
                 break;
             }
 
@@ -121,13 +135,7 @@ int main() {
                 //Si la liste est vide
                 if(isEmptyContact(MyContactList, 0)) {
                     printf("Vous n'avez aucun contact\n");
-                    int val2;
-                    printf("Entrer 0 pour revenir en arrière.\n");
-                    do {
-                        printf("$ ");
-                        scanf("%d", &val2);
-                    } while (val2 != 0);
-                    printf("\n\n");
+                    int val1 = SecureAdd(2);
                 }
 
                 //Si la liste n'est pas vide
